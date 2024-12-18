c13e08c4694688b40bf3b030b7ec775777bc1dac
Dimeji <dimejiogunyoye@googlemail.com>
2024-12-18 23:05:36 +0000

    Day 18 - performance improvement

--- day18/day18.py
+++ day18/day18.py
@@ -31,24 +31,28 @@ def __initialise_grid() -> dict:
     return grid
 
 
-def __bfs(start, end, grid) -> int:
+def __bfs(start, end, grid) -> tuple:
     queue, visited = deque(), set()
-    queue.append((start, 0))
+    path = set()
+    path.add(start)
+    queue.append((start, 0, path))
 
     while len(queue) != 0:
         current = queue.popleft()
-        (x, y), steps = current[0], current[1]
+        (x, y), steps, path = current[0], current[1], current[2]
 
         if (x, y) == end:
-            return steps
+            return steps, path
 
         neighbors = [(x, y - 1), (x - 1, y), (x, y + 1), (x + 1, y)]
         for n in neighbors:
             if n in grid and grid[n] != '#' and n not in visited:
-                queue.append((n, steps + 1))
+                new_path = set(path)
+                new_path.add(n)
+                queue.append((n, steps + 1, new_path))
                 visited.add(n)
 
-    return -1
+    return -1, set()
 
 
 def part_one(data) -> int:
@@ -58,7 +62,7 @@ def part_one(data) -> int:
     for f in falling_bytes[0:1024]:
         grid[f] = '#'
 
-    return __bfs((0, 0), (BOUND, BOUND), grid)
+    return __bfs((0, 0), (BOUND, BOUND), grid)[0]
 
 
 def part_two(data) -> str:
@@ -68,10 +72,14 @@ def part_two(data) -> str:
     for f in falling_bytes[0:1024]:
         grid[f] = '#'
 
+    _, path = __bfs((0,0), (BOUND, BOUND), grid)
+
     for i in range(1024, len(falling_bytes)):
         grid[falling_bytes[i]] = '#'
-        if __bfs((0,0), (BOUND, BOUND), grid) == -1:
-            return str(falling_bytes[i])
+        if falling_bytes[i] in path:
+            steps, path = __bfs((0,0), (BOUND, BOUND), grid)
+            if steps == -1:
+                return str(falling_bytes[i])
 
     raise Exception("No solution found!")
 
