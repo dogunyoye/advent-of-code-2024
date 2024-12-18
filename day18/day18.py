ec84ea31868d3793342f3ac956a501296940c3b8
Dimeji Ogunyoye <dimeji.ogunyoye@diffusiondata.com>
2024-12-18 11:21:47 +0000

    Day 18 - part 2

--- day18/day18.py
+++ day18/day18.py
@@ -48,7 +48,7 @@ def __bfs(start, end, grid) -> int:
                 queue.append((n, steps + 1))
                 visited.add(n)
 
-    raise Exception("No solution found!")
+    return -1
 
 
 def part_one(data) -> int:
@@ -61,8 +61,19 @@ def part_one(data) -> int:
     return __bfs((0, 0), (BOUND, BOUND), grid)
 
 
-def part_two(data) -> int:
-    return 0
+def part_two(data) -> str:
+    falling_bytes = __build_falling_bytes_list(data)
+    grid = __initialise_grid()
+
+    for f in falling_bytes[0:1024]:
+        grid[f] = '#'
+
+    for i in range(1024, len(falling_bytes)):
+        grid[falling_bytes[i]] = '#'
+        if __bfs((0,0), (BOUND, BOUND), grid) == -1:
+            return str(falling_bytes[i])
+
+    raise Exception("No solution found!")
 
 
 def main() -> int:
